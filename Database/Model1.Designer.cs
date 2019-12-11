876c045716ae1c23870ad1a96b7fea7ec2b49922
ekjoh <ekjohnson99@gmail.com>
2019-12-11 16:46:18 -0500

    Updated Model, Password now Varchar(200)

--- Database/Model1.Designer.cs
+++ Database/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\School\Semester7\FinalProject\Final\YogaStudioHelper\Database\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ekjoh\source\repos\YogaStudioHelper\Database\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
