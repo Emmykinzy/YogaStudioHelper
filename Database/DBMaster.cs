d210cb6cf58574aa8cdde82feae125f50ac62f20
Benjamin Caron-Lortie <32286421+ben-cl@users.noreply.github.com>
2019-12-11 10:41:15 -0500

    DBMaster class created

---
+++ Database/DBMaster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+   
+
+    class DBMaster
+    {
+        private static yogadbEntities myDb;
+
+        public static IEnumerable<Yoga_User> getCustomer()
+        {
+            return myDb.Yoga_User.ToList();
+        }
+
+
+    }
+}
