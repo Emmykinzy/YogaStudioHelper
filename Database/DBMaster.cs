198a0d737e0627076a383119a9c1696b818118e8
ekjoh <ekjohnson99@gmail.com>
2019-12-11 11:22:32 -0500

    Added Db User methods

--- Database/DBMaster.cs
+++ Database/DBMaster.cs
@@ -10,13 +10,71 @@ namespace Database
 
     class DBMaster
     {
-        private static yogadbEntities myDb;
+         yogadbEntities myDb = new yogadbEntities();
 
-        public static IEnumerable<Yoga_User> getCustomer()
+        // User Get Methods 
+        public IEnumerable<Yoga_User> getUsers()
         {
             return myDb.Yoga_User.ToList();
         }
 
+        public Yoga_User getUserById(int id)
+        {
+            return myDb.Yoga_User.Where(x => x.U_Id == id).Single();
+        }
 
+        public IEnumerable<Yoga_User> getUserByEmail(string email)
+        {
+            return myDb.Yoga_User.Where(x => x.U_Email.Contains(email));
+        }
+
+        public IEnumerable<Yoga_User> getUserByLastName(string lName)
+        {
+            return myDb.Yoga_User.Where(x => x.U_Last_Name.Contains(lName));
+        }
+
+        public IEnumerable<Yoga_User> getUserByFirstName(string fName)
+        {
+            return myDb.Yoga_User.Where(x => x.U_First_Name.Contains(fName));
+        }
+
+        public IEnumerable<Yoga_User> getUserByActive(bool act)
+        {
+            return myDb.Yoga_User.Where(x => x.Active == act);
+        }
+
+        public IEnumerable<Yoga_User> getUserByPhone(string phone)
+        {
+            return myDb.Yoga_User.Where(x => x.U_Phone.Contains(phone));
+        }
+
+        public IEnumerable<Yoga_User> getUserByRole(int role)
+        {
+            return myDb.Yoga_User.Where(x => x.Roles_Id == role);
+        }
+
+        // User Get Methods 
+        public void CreateUser()
+        {
+            YogaUser m = new YogaUser(); 
+            Yoga_User y = new Yoga_User(YogaUser);
+
+            y.Roles_Id = role;
+            y.U_First_Name = fn;
+            y.U_Last_Name = ln;
+            y.U_Email = email;
+            y.U_Password = password;
+            y.U_Phone = phone;
+            y.Availability = availability;
+            y.U_Birthday = birthday;
+
+            myDb.Yoga_User.Add(y);
+        }
+
+        public void UpdateUser(int id)
+        {
+           var u = myDb.Yoga_User.Where(x => x.U_Id == id).Single();
+          
+        }
     }
 }
