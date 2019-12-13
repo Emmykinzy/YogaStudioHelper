49538a07c3d6353f03c7ba305dfe3a1586c1edc2
Benjamin Caron-Lortie <32286421+ben-cl@users.noreply.github.com>
2019-12-12 21:47:51 -0500

    test paypal en development

---
+++ YogaStudioHelper/Models/PaypalConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using PayPal.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YogaStudioHelper.Models
+{
+    public class PaypalConfiguration
+    {
+
+        public readonly static string ClientId;
+        public readonly static string ClientSecret;
+        
+        static PaypalConfiguration()
+        {
+            // TODO
+            var config = GetConfig();
+            ClientId = config["clientId"];
+            ClientSecret = config["clientSecret"];
+        }
+
+        public static Dictionary<string,string> GetConfig()
+        {
+            return PayPal.Api.ConfigManager.Instance.GetProperties(); 
+
+        }
+
+        // Create access token 
+        private static string GetAccessToken()
+        {
+            string accessToken = new OAuthTokenCredential(ClientId, ClientSecret, GetConfig()).GetAccessToken();
+            return accessToken;
+        }
+
+
+        //This will return api Context Object 
+        public static APIContext GetAPIContext()
+        {
+            var apiContext = new APIContext(GetAccessToken());
+            apiContext.Config = GetConfig();
+            return apiContext;
+        }
+
+
+
+    }
+}
